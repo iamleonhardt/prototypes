2a291d18362935dfe680471d576784b45aba7617
Bill Leonhardt <iamleonhardt@gmail.com>
2016-07-01 14:54:07 -0700

    JS Event Delegation - Bill Leonhardt

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,17 +1,22 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
-
+//      It's going to console log "Delegated Button #5 Handler"
 // - What happened?
-
+//      It worked.
 // - Why?
+//      Because I used "#list" as the parent element and "button" as the child.
 
 //========== Write your code below ===========//
-$(document).ready (function() {
-    $("#list button").on("click", function() {
-        console.log ( $(this).text());
+$(document).ready(function () {
+    $("#list").on("click", "button", function () {
+        console.log($(this).text());
     });
+
     var newButton = $("<li><button style='margin-top: 10px'>Delegated Button #5 Handler</button></li>");
     $("#list").append(newButton);
+
+    var googleButton = $("<li><button style='margin-top: 10px'><a href=http://google.com alt=google.com target=_blank style='text-decoration: none; color: black'>For Shits and Googles</a></button></li>");
+    $("#list").append(googleButton);
 });
 
