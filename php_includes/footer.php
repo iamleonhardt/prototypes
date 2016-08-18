5d73a1496bdb439791647f967e188fa2f1db56a7
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-18 11:28:33 -0700

    PHP Includes - Bill Leonhardt

---
+++ php_includes/footer.php
@@ -0,0 +1,8 @@
+
+<nav id="site_map">
+    <ul>
+        <li>Location
+        <li>Contact Us</li>
+        <li>Hours</li>
+    </ul>
+</nav>
---
+++ php_includes/main.php
@@ -0,0 +1,5 @@
+
+Main Content<br>
+Spiders<br>
+<img src="https://www.creepycrawlies.info/images/spider3-1.jpg">
+
