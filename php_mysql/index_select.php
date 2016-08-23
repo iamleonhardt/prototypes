fa7a1d4e0f20249912194027941ece513310e2ed
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-23 07:08:48 -0700

    php mysql

--- php_mysql/index_select.php
+++ php_mysql/index_select.php
@@ -1,8 +1,20 @@
 <!-- index_select.php -->
 
 <?php
-require_once('mysql_connect.php');
+    require_once('mysql_connect.php');
 
-$query = "SELECT * FROM todo_items";
+    $query = 'SELECT * FROM `todo_items`';
+
+    $result = mysqli_query($conn, $query);
+    print_r($result);
 
 ?>
+
+
+
+<!--connect (hide the config!)-->
+<!--make query-->
+<!--send query-->
+<!--check if result is valid-->
+<!--check if there are rows-->
+<!--go through each row-->
