d93c702e2f2407d5506af886aea80426cbcbce2c
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-24 18:27:57 -0700

    php user auth basic, feature set 1

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,45 @@
 <!--login_form.php-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" name="username" placeholder="user name" id="username">
+    <input type="password" name="password" placeholder="password" id="password">
+    <button type="button" class="login">Login</button>
+
+<script>
+    $(document).ready(function(){
+        $('.login').click(function(){
+            var user = $('#username').val();
+            var password = $('#password').val();
+            console.log('it was clicked');
+            console.log('user is : ', user);
+            $.ajax({
+                url: 'login_handler.php',
+                data: {
+                    username: user,
+                    password: password
+                },
+                cache: false,
+                method: 'post',
+                dataType: 'text',
+                success: function(response){
+                    console.log(response);
+                }
+            })
+        });
+    });
+</script>
+</body>
+</html>
+
+
+
+
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,29 @@
 <!--login_handler.php-->
+<?php
+session_start();
+
+$user_info = [
+    ['id' => 0, 'username' => 'Ashley', 'password' => 'stuff'],
+    ['id' => 1, 'username' => 'Barb', 'password' => 'stuff'],
+    ['id' => 2, 'username' => 'Chelsea', 'password' => 'stuff'],
+    ['id' => 3, 'username' => 'April', 'password' => 'stuff'],
+    ['id' => 4, 'username' => 'Riley', 'password' => 'stuff'],
+];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+for($i = 0; $i < count($user_info); $i++){
+    if ($username == $user_info[$i]['username']){
+        print ' **it worked!** ';
+    }
+    else {
+        print ' it didnt work ';
+    }
+}
+
+//if(!empty($_POST['username']) && (!empty($_POST['password'])){
+//    if($user_info[$_POST['username']]['password'])
+//    })
+
+?>
