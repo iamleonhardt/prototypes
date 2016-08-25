e1b999fff1b70a765bfff27b80c093a37d4e9a40
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-25 10:30:42 -0700

    php user auth, working on feat 2

--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -27,4 +27,19 @@ for($i = 0; $i < count($user_info); $i++){
 //    if($user_info[$_POST['username']]['password'])
 //    })
 
+
+$output = [
+    'success' => true,
+];
+
+
 ?>
+
+
+Make a php array called $output
+Give it an element of success. Set it to true if login was successful, false if not
+If successful, give it an element of user_id. Set it to the logging in user
+Give it an element of message. Put an appropriate login message depending on login result
+Use json_encode() to encode $output into a string, $output_string
+Print: $output_string
+Remember: If any other text is printed out alongside the json, the json conversion in the AJAX call will fail. The only thing printed can be json
