8ec048030a3e881b90fe56504d7a0d2c9ced414d
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-25 11:06:39 -0700

    feature set 1 complete

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,1 +1,11 @@
 <!-- File Handler -->
+<pre>
+<?php
+
+//is_dir(uploads);
+
+print_r ($_FILES[upload_file]);
+print_r ($_POST);
+
+?>
+</pre>
--- php_file_upload/uploader_form.php
+++ php_file_upload/uploader_form.php
@@ -3,7 +3,10 @@
 <head>
 </head>
 <body>
-	<form id="file_upload">
+	<form id="file_upload" action="file_handler.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="upload_file">
+		<input type="text" name="description" placeholder="description">
+		<button>Submit</button>
 		
 	</form>
 </body>
