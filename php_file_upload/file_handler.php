fec15b6ef7822e3bd2897759ddc7760e4e2189ef
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-29 10:46:34 -0700

    php file uploads

--- php_file_upload/file_handler.php
+++ php_file_upload/file_handler.php
@@ -1,11 +1,49 @@
 <!-- File Handler -->
 <pre>
 <?php
+$uploadFolder = 'uploads/';
+if (!file_exists($uploadFolder)) {
+    mkdir('uploads');
+}
+$errors = [];
+$messages = [];
+$allowed = array('gif', 'jpeg', 'jpg', 'png');
+$filename = $_FILES['upload_file']['name'];
+$tempname = $_FILES['upload_file']['tmp_name'];
 
-//is_dir(uploads);
+$ext = pathinfo($_FILES['upload_file']['name'])['extension'];
+$maxsize = 2000000;
+if ($_FILES['upload_file']['size'] < $maxsize) {
+    $messages[] = 'filesize is good yo';
+} else {
+    $errors[] = 'File size is too large. File must be less than 2 megabytes.';
+}
+if (in_array($ext, $allowed)) {
+   $messages[] = 'ext is valid';
+} else {
+    $errors[] =  'ext is not valid';
+}
+if (count($errors) == 0) {
+    if(move_uploaded_file($tempname, $uploadFolder . $filename)){
+        $messages[] = 'the upload worked';
+    }
+    else {
+        $errors[] = 'the upload didnt work';
+    };
+}
+else {
+    $errors[] =  'filesize or extension are not valid';
+}
+print ('errors : ' . '<br>');
+print_r($errors);
+print ('messages : ' . '<br>');
+print_r($messages);
 
-print_r ($_FILES[upload_file]);
-print_r ($_POST);
-
+$images = glob($uploadFolder."*.jpg");
+foreach($images as $image) {
+    echo '<img style="width:200px; float:left" src="'.$image.'" /><br />';
+}
 ?>
 </pre>
+
+
--- php_file_upload/uploader_ajax.php
+++ php_file_upload/uploader_ajax.php
@@ -1,10 +1,31 @@
 <!doctype html>
 <html>
 <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#submit').click(function(){
+                var formEle = $('#ajax-upload');
+
+                $.ajax({
+                    url: 'file_handler.php',
+                    processData: false,
+                    contentType: false,
+                    method: 'post',
+                    data: new FormData(formEle[0]),
+                    success: function(result){
+                        console.log(result);
+                    }
+                })
+            })
+        })
+    </script>
 </head>
 <body>
-	<form id="file_upload">
-		
-	</form>
+<form id="ajax-upload" action="file_handler.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file">
+    <input type="text" name="description" placeholder="description">
+    <button type="button" id="submit">Submit</button>
+</form>
 </body>
 </html>
